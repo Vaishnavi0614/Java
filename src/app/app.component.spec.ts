1e39638a659b3275dfa9097ddbb24050ac413626
shrkha60 <shreyas.khambayate@siemens.com>
2023-05-30 20:26:54 +0530

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,6 +17,7 @@ describe('AppComponent', () => {
   });
 
   it(`should have as title 'SecondProject'`, () => {
+    console.log("hello");
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('SecondProject');
